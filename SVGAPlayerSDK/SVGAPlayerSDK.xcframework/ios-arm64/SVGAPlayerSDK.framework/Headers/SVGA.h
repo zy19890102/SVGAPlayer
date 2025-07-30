95fea3b319343e6ce322994e720c33704ba0bb29
stary <zhangyu19890102@126.com>
2025-07-30 12:24:19 +0800

    feat： svga

---
+++ SVGAPlayerSDK/SVGAPlayerSDK.xcframework/ios-arm64/SVGAPlayerSDK.framework/Headers/SVGA.h
@@ -0,0 +1,18 @@
+//
+//  SVGA.h
+//  SVGAPlayer
+//
+//  Created by 崔明辉 on 16/6/17.
+//  Copyright © 2016年 UED Center. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SVGAPlayerSDK/SVGAParser.h>
+#import <SVGAPlayerSDK/SVGAPlayer.h>
+#import <SVGAPlayerSDK/SVGAImageView.h>
+#import <SVGAPlayerSDK/SVGAVideoEntity.h>
+#import <SVGAPlayerSDK/SVGAExporter.h>
+
+@interface SVGA : NSObject
+
+@end
---
+++ SVGAPlayerSDKTarget/SVGAPlayerSDK.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by WXJZ on 2024/5/16.
+//
+
+import Foundation
