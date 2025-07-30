15457bf488e9fec744641883549d26bc2c36be6a
stary <zhangyu19890102@126.com>
2025-07-30 14:36:18 +0800

    sign

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "SVGAPlayerSDK",
+    name: "SVGAPlayer",
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SVGAPlayerSDK",
-            targets: ["SVGAPlayerSDKTarget"]),
+            name: "SVGAPlayer",
+            targets: ["SVGAPlayerTarget"]),
     ],
     dependencies: [.package(url: "https://github.com/ZipArchive/ZipArchive.git", exact: "2.4.3"),
                    .package(url: "https://github.com/zy19890102/Protobuf", exact: "1.1.0")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "SVGAPlayerSDK", path: "SVGAPlayerSDK/SVGAPlayerSDK.xcframework"),
-        .target(name: "SVGAPlayerSDKTarget",
-                dependencies: [.target(name: "SVGAPlayerSDK"),
+        .binaryTarget(name: "SVGAPlayer", path: "SVGAPlayer/SVGAPlayerSDK.xcframework"),
+        .target(name: "SVGAPlayerTarget",
+                dependencies: [.target(name: "SVGAPlayer"),
                                .product(name: "Protobuf", package: "Protobuf"),
                                .product(name: "ZipArchive", package: "ZipArchive")],
-                path: "SVGAPlayerSDKTarget")
+                path: "SVGAPlayerTarget")
     ]
 )
 
